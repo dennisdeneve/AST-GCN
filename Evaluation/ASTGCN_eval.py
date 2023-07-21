4d961c78a474dd3365f191c0ae957ed7a053906c
dennis-hammerschlag <dennishammerschlag01@gmail.com>
2023-07-21 17:14:04 +0200

    Refactored ASTGCN_eval.py more

--- Evaluation/ASTGCN_eval.py
+++ Evaluation/ASTGCN_eval.py
@@ -2,66 +2,45 @@ import pandas as pd
 from Utils.utils import create_file_if_not_exists
 from Evaluation.metrics import SMAPE, MSE, RMSE, MAE
 
-def evalASTGCN(config):
-    """
-    Calculates the ASTGCN model's performance on the test set for each station. These metrics are written to a file
-    for each station. The predictions are read from the results file for each station. The targets are pulled from
-    the weather stations' data sets. The MSE, MAE, RMSE, and SMAPE metrics are calculated on all forecasting
-    horizons(3, 6, 9, 12, and 24) for each individual weather station. The metrics for each station, across all
-    forecasting horizons are then written to a text file.
+def get_file_paths(station, horizon, model='ASTGCN'):
+    return {
+        "yhat": f'Results/{model}/{horizon} Hour Forecast/{station}/Predictions/result.csv',
+        "target": f'Results/{model}/{horizon} Hour Forecast/{station}/Targets/target.csv',
+        "metrics": f'Results/{model}/{horizon} Hour Forecast/{station}/Metrics/metrics.txt',
+        "actual_vs_predicted": f'Results/{model}/{horizon} Hour Forecast/{station}/Metrics/actual_vs_predicted.txt'
+    }
 
-    Parameters:
-        stations - List of the weather stations.
-        model - Whether these metrics are being calculated for the LSTM or TCN model
-    """
-    model = 'ASTGCN'
+def evalASTGCN(config):
     stations = config['stations']['default']
     horizons = config['forecasting_horizons']['default']
     for station in stations:
         for horizon in horizons:
+            print(f'ASTGCN evaluation started at {station} for the horizon of {horizon}')
+            paths = get_file_paths(station, horizon)
             try:
-                print('ASTGCN evaluation started at' , str(station)+' for the horizon of ' ,str(horizon)) 
-                 # Set the file paths for predictions, targets, and metrics
-                yhat_path = f'Results/ASTGCN/{horizon} Hour Forecast/{station}/Predictions/result.csv'
-                target_path = f'Results/ASTGCN/{horizon} Hour Forecast/{station}/Targets/target.csv'
-                metric_file = f'Results/ASTGCN/{horizon} Hour Forecast/{station}/Metrics/metrics.txt'
-                actual_vs_predicted_file = f'Results/ASTGCN/{horizon} Hour Forecast/{station}/Metrics/actual_vs_predicted.txt'
-                create_file_if_not_exists(yhat_path)
-                create_file_if_not_exists(target_path)
-                create_file_if_not_exists(metric_file)
-                create_file_if_not_exists(actual_vs_predicted_file)
-                
+                for path in paths.values():
+                    create_file_if_not_exists(path)
                 # Read the predictions and targets from the CSV files
-                preds = pd.read_csv(yhat_path).drop(['Unnamed: 0'], axis=1)
-                targets = pd.read_csv(target_path).drop(['Unnamed: 0'], axis=1)
-                
-                # Create a DataFrame of actual vs predicted values
+                preds = pd.read_csv(paths['yhat']).drop(['Unnamed: 0'], axis=1)
+                targets = pd.read_csv(paths['target']).drop(['Unnamed: 0'], axis=1)
+                 # Create a DataFrame of actual vs predicted values & Save it
                 actual_vs_predicted = pd.DataFrame({'Actual': targets.values.flatten(), 'Predicted': preds.values.flatten()})
-                # Save to a text file
-                actual_vs_predicted.to_csv(actual_vs_predicted_file, index=False)
+                actual_vs_predicted.to_csv(paths['actual_vs_predicted'], index=False)
+                # Calculate the metrics &  Write the metrics to the files
+                metrics = {
+                    'MSE': MSE(targets.values, preds.values),
+                    'RMSE': RMSE(targets.values, preds.values),
+                    'MAE': MAE(targets.values, preds.values),
+                    'SMAPE': SMAPE(targets.values, preds.values)
+                }
+                with open(paths['metrics'], 'w') as metric_file:
+                    for name, value in metrics.items():
+                        metric_file.write(f'This is the {name}: {value}\n')
 
-                # Calculate the metrics
-                mse = MSE(targets.values, preds.values)
-                rmse = RMSE(targets.values, preds.values)
-                mae = MAE(targets.values, preds.values)
-                smape = SMAPE(targets.values, preds.values)
-
-                # Write the metrics to the metric file
-                with open(metric_file, 'w') as metric:
-                    metric.write('This is the MSE: {}\n'.format(mse))
-                    metric.write('This is the MAE: {}\n'.format(mae))
-                    metric.write('This is the RMSE: {}\n'.format(rmse))
-                    metric.write('This is the SMAPE: {}\n'.format(smape))
-                
-                print(f'ASTGCN evaluation done at {station} for the horizon of {horizon} ') 
-                print(f'And was saved to Results/ASTGCN/{horizon} Hour Forecast/{station}/Metrics/metrics.txt' ) 
-                
-                # Print the metrics for the current station and horizon length
-                print('SMAPE: {0} at the {1} station forecasting {2} hours ahead.'.format(smape, station, horizon))
-                print('MSE: {0} at the {1} station forecasting {2} hours ahead.'.format(mse, station, horizon))
-                print('MAE: {0} at the {1} station forecasting {2} hours ahead.'.format(mae, station, horizon))
-                print('RMSE: {0} at the {1} station forecasting {2} hours ahead.'.format(rmse, station, horizon))
-                print('')
-            except IOError as e :
-                print('Error! Unable to read data or write metrics:', str(e))
-    print('Finished evaluation of TGCN error metrics for all stations.')  
+                print(f'ASTGCN evaluation done at {station} for the horizon of {horizon}')
+                print(f'And was saved to {paths["metrics"]}')
+                for name, value in metrics.items():
+                    print(f'{name}: {value} at the {station} station forecasting {horizon} hours ahead.')
+            except Exception as e:
+                print(f'Error! Unable to read data or write metrics: {str(e)}')
+    print('Finished evaluation of TGCN error metrics for all stations.')
