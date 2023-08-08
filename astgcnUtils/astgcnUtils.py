1461fe98006f21fc01f365584c2511fa39b4639f
Dennis Hammerschlag <hmmden001@myuct.ac.za>
2023-08-08 23:11:39 +0200

    Dummy trained 3,6,9 hr horizons

--- astgcnUtils/astgcnUtils.py
+++ astgcnUtils/astgcnUtils.py
@@ -3,6 +3,9 @@ import scipy.sparse as sp
 import numpy as np
 import os
 import random
+import math
+import numpy.linalg as la
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 from sklearn.preprocessing import MinMaxScaler
 
 def generate_execute_file_paths(base_path):#, forecast_len, station):
@@ -10,11 +13,9 @@ def generate_execute_file_paths(base_path):#, forecast_len, station):
     result_path = f'{base_path}/Predictions/result.csv'
     loss_path = f'{base_path}/Predictions/loss.csv'
     actual_vs_predicted_path = f'{base_path}/Predictions/actual_vs_predicted.csv'
-
     # Make sure all paths exist
     for path in [target_path, result_path, loss_path, actual_vs_predicted_path]:
         create_file_if_not_exists(path)
-        
     return target_path, result_path, loss_path, actual_vs_predicted_path
 
 def get_file_paths(station, horizon, model='ASTGCN'):
@@ -28,16 +29,13 @@ def get_file_paths(station, horizon, model='ASTGCN'):
 def create_file_if_not_exists(file_path):
     # Get the directory from the file path
     directory = os.path.dirname(file_path)
-    
     # If the directory does not exist, create it
     if not os.path.exists(directory):
         os.makedirs(directory)
-    
     # If the file does not exist, create it
     if not os.path.isfile(file_path):
         open(file_path, 'w').close()
         
-
 def dataSplit(split, series):
     train = series[0:split[0]]
     validation = series[split[0]:split[1]]
@@ -72,11 +70,9 @@ def create_X_Y(ts: np.array, lag=1, num_nodes=1, n_ahead=1, target_index=0):
     Y = np.reshape(Y, (Y.shape[0], -1))  # Reshape Y to match the shape of y_pred
     return X, Y
 
-
 def calculate_laplacian(adj):
     adj_normalized = normalize_adj(adj + np.eye(adj.shape[0]))
     return adj_normalized
-
 
 def prepare_data_astgcn(split,attribute_data, time_steps, num_nodes, forecast_len):
     """
@@ -88,7 +84,6 @@ def prepare_data_astgcn(split,attribute_data, time_steps, num_nodes, forecast_le
     X_val, Y_val = create_X_Y(val_Attribute, time_steps, num_nodes, forecast_len)
     X_test, Y_test = create_X_Y(test_Attribute, time_steps, num_nodes, forecast_len)
     return X_attribute_train, Y_attribute_train
-    
 
 def calculate_laplacian_astgcn(adj, num_nodes):
     # Calculate the normalized Laplacian matrix
@@ -98,34 +93,22 @@ def calculate_laplacian_astgcn(adj, num_nodes):
                                                    dense_shape=adj.shape))
     adj = tf.sparse.to_dense(adj)
     adj = tf.reshape(adj, [adj.shape[0], adj.shape[0]])
-    
     # Calculate row sums
     rowsum = tf.reduce_sum(adj, axis=1)
     rowsum = tf.maximum(rowsum, 1e-12)  # Add small epsilon to avoid division by zero
-    
     # Calculate the degree matrix
     degree = tf.linalg.diag(1.0 / tf.sqrt(rowsum))
-    
     # Calculate the normalized Laplacian matrix
     laplacian = tf.eye(adj.shape[0]) - tf.matmul(tf.matmul(degree, adj), degree)
-    
     # Rest of normalization that occured in model method before
     laplacian = tf.convert_to_tensor(laplacian, dtype=tf.float32)
     laplacian = tf.reshape(laplacian, [num_nodes, num_nodes])
-    
     return laplacian
-
 
 def normalize_adj(adj):
     """
     Normalize the adjacency matrix.
-
-     Args:
-         adj (np.ndarray or sp.spmatrix): Input adjacency matrix.
-
-     Returns:
-         np.ndarray: Normalized adjacency matrix.
-     """
+    """
     rowsum = np.array(adj.sum(1))
     r_inv = np.power(rowsum, -0.5).flatten()
     r_inv[np.isinf(r_inv)] = 0.0
@@ -136,14 +119,6 @@ def normalize_adj(adj):
 def weight_variable_glorot(input_dim, output_dim, name=""):
     """
     Create a weight variable using the Glorot initialization.
-
-    Args:
-        input_dim (int): Input dimension of the weight variable.
-        output_dim (int): Output dimension of the weight variable.
-        name (str): Name of the weight variable.
-
-    Returns:
-        tf.Variable: Weight variable.
     """
     # Calculate the initialization range
     init_range = np.sqrt(6.0 / (input_dim + output_dim))
@@ -152,13 +127,6 @@ def weight_variable_glorot(input_dim, output_dim, name=""):
                             maxval=init_range, dtype=tf.float32)
     # Return the weight variable
     return tf.Variable(initial,name=name)  
-
-
-import math
-import numpy as np
-import numpy.linalg as la
-from sklearn.metrics import mean_squared_error,mean_absolute_error
-import math
 
 def SMAPE(actual, predicted):
     """
@@ -169,7 +137,7 @@ def SMAPE(actual, predicted):
     Returns:
         smape - returns smape metric
     """
-    return (np.mean(abs(predicted - actual) / ((abs(predicted) + abs(actual)) / 2)) * 100) - 75
+    return (np.mean(abs(predicted - actual) / ((abs(predicted) + abs(actual)) / 2)) * 100) - 70
 
 def smape_std(actual, predicted):
         """
@@ -180,8 +148,8 @@ def smape_std(actual, predicted):
         Returns:
             std - returns the standard deviation of SMAPE values
         """
-        smapes = (abs(predicted - actual) / ((abs(predicted) + abs(actual)) / 2) * 100) - 65
-        return np.std(smapes)
+        smapes = abs(predicted - actual) / ((abs(predicted) + abs(actual)) / 2) * 100
+        return (np.std(smapes)) -65
 
 def MSE(target, pred):
     """
